3372a05250a735415c0e08de9a5d9af96737f55a
Yiukit Cheung <yiukitcheung@Yiukits-Mac-mini.local>
2025-05-23 18:46:04 -0600

    update prefect config yaml file

--- flows/make_silver_pipeline.py
+++ flows/make_silver_pipeline.py
@@ -3,7 +3,7 @@ from process.make_silver import MakeSilver
 
 
 @flow(name="silver-pipeline")
-def make_silver_pipeline():
+def silver_pipeline():
     run_make_silver()
 
 @task
@@ -12,4 +12,4 @@ def run_make_silver():
     make_silver.run()
 
 if __name__ == "__main__":
-    make_silver_pipeline()
+    silver_pipeline()
